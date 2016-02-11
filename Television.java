30d66078497889aa05e2cbb1fa3cf675ac7f25ca
Cornelius Toole <cornelius.toole@gmail.com>
2016-02-11 08:59:58 -0600

    Create Television.java
    completed steps 1 - 7 of the Task 1 in the lab manual

---
+++ Television.java
@@ -0,0 +1,12 @@
+public class Television {
+// The purpose of this class is to model a television
+// Cornelius Toole 02/11/16
+
+private final String MANUFACTURER;
+private final int SCREEN_SIZE;
+
+private boolean powerOn;
+private int channel;
+private int volume;
+
+}
